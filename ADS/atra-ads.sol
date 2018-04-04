aef5201aa00033fd0b399c070f6c4e4fd233b152
Dillon Vincent <dvincent@douche-bag.local>
2018-04-04 11:57:00 -0700

    input validation

--- ADS/atra-ads.sol
+++ ADS/atra-ads.sol
@@ -13,13 +13,13 @@ contract ADS {
         address owner; // address that owns/created and can modify route
         address newOwner; // used to transfer ownership
         RouteData current; // current contract data
-        RouteData upcoming; // upcomin contract data
+        RouteData next; // next contract data
     }
     
     // Declare Storage 
     Route[] public Routes;
     mapping(bytes32 => uint) public ContractNamesToRoutes; // keccak256([name])
-    mapping(address => uint[]) public OwnersToRoutes; 
+    mapping(address => uint[]) public OwnersToRoutes; // help pull all a users routes for the ui
 
     //Constructor
     function ADS() public {
@@ -29,16 +29,16 @@ contract ADS {
         ContractNamesToRoutes[keccak256('ADS')] = Routes.push(Route('ADS', 0, msg.sender, msg.sender, RouteData('atra.io/abi/ads',this), RouteData('atra.io/abi/ads',this))) -1;
     }
 
-    function Get(string routeName) public view returns(string name, address owner, uint currentExpiration, address currentContractAddress, string currentAbiLocation, address upcomingAddress, string upcomingAbiLocation) {
+    function Get(string routeName) public view returns(string name, address owner, uint currentExpiration, address currentContractAddress, string currentAbiLocation, address nextAddress, string nextAbiLocation) {
         Route memory route = Routes[ContractNamesToRoutes[keccak256(routeName)]];
-        return (route.name, route.owner, route.currentExpiration, route.current.contractAddress, route.current.abiLocation, route.upcoming.contractAddress, route.upcoming.abiLocation);
+        return (route.name, route.owner, route.currentExpiration, route.current.contractAddress, route.current.abiLocation, route.next.contractAddress, route.next.abiLocation);
     }
     
     //returns the valid contract address
     function GetAddress(string routeName) public view returns(address validAddress){
         Route memory route = Routes[ContractNamesToRoutes[keccak256(routeName)]];
-        if(route.currentExpiration < now){ // the current contract has expired use upcoming
-            return route.upcoming.contractAddress;
+        if(route.currentExpiration < now){ // the current contract has expired use next
+            return route.next.contractAddress;
         }else{
             return route.current.contractAddress;  
         }
@@ -48,39 +48,41 @@ contract ADS {
         Route memory route = Routes[ContractNamesToRoutes[keccak256(routeName)]];
         return route.current.contractAddress;
     }
-    function GetUpcomingAddress(string routeName) public view returns(address upcomingContractAddress) {
+    function GetNextAddress(string routeName) public view returns(address nextContractAddress) {
         Route memory route = Routes[ContractNamesToRoutes[keccak256(routeName)]];
-        return route.upcoming.contractAddress;
+        return route.next.contractAddress;
     }
     
-
-    // creating a route is free
-    // you can create a route with the intent to update it in the near future, by batching this logic it saves tx fees
-    function Create(string name, address currentAddress, string currentAbiLocation, address upcomingAddress, string upcomingAbiLocation, address newOwner, uint currentExpiration) public returns(uint newRouteId) {
-        require(bytes(name).length > 0);
+    function Create(string name, address currentAddress, string currentAbiLocation, address nextAddress, string nextAbiLocation, address newOwner, uint currentExpiration) public returns(uint newRouteId) {
+        // keep the memory from getting out of hand
+        require(bytes(name).length > 0 && bytes(name).length <= 100 && bytes(currentAbiLocation).length <= 256 && bytes(nextAbiLocation).length <= 256);
+        
         require(ContractNamesToRoutes[keccak256(name)] == 0);
         // "x","0xd26114cd6EE289AccF82350c8d8487fedB8A0C07", "github.com/omg/abi", "0xd26114cd6EE289AccF82350c8d8487fedB8A0C07", "github.com/omg/abi", "0x0", 0
         RouteData memory current = RouteData(currentAbiLocation, currentAddress);
-        RouteData memory upcoming = RouteData(upcomingAbiLocation, upcomingAddress);
-        return ContractNamesToRoutes[keccak256(name)] = Routes.push(Route(name, now + currentExpiration, msg.sender, newOwner, current, upcoming)) -1;
+        RouteData memory next = RouteData(nextAbiLocation, nextAddress);
+        return ContractNamesToRoutes[keccak256(name)] = Routes.push(Route(name, now + currentExpiration, msg.sender, newOwner, current, next)) -1;
     }
 
     function Update(string name, uint currentExpiration, address newContractAddress, string newAbiLocation) public returns(bool success) {
+        //dont require name validation since we aren't storing it
+        require(bytes(newAbiLocation).length <= 256);
+       
         uint routeId = ContractNamesToRoutes[keccak256(name)];
         require(Routes[routeId].owner == msg.sender); //require sender to be owner to update
         Routes[routeId].currentExpiration = currentExpiration == 0 ? 0 : now + currentExpiration; // update when the current contract expires to epoch
-        Routes[routeId].upcoming.contractAddress = newContractAddress; // update upcoming address
-        Routes[routeId].upcoming.abiLocation = newAbiLocation; // update upcoming abi location
+        Routes[routeId].next.contractAddress = newContractAddress; // update next address
+        Routes[routeId].next.abiLocation = newAbiLocation; // update next abi location
         return true; // return success
     }
     
-    //This function will switch over the upcoming route to the current route data if the current route has expired, and set expiration
+    //This function will switch over the next route to the current route data if the current route has expired, and set expiration
     function Live(string name) public returns(bool success) {
         uint routeId = ContractNamesToRoutes[keccak256(name)]; // get route Id by route name
         require(Routes[routeId].owner == msg.sender); //require sender to be owner to update
         //require there to be an expiration time for the current contract and it be expired
         require(Routes[routeId].currentExpiration != 0 && Routes[routeId].currentExpiration < now);
-        Routes[routeId].current = Routes[routeId].upcoming; // update current with contents of upcoming
+        Routes[routeId].current = Routes[routeId].next; // update current with contents of next
         Routes[routeId].currentExpiration = 0; // set the current contract to never expire
         return true; // return success
     }
@@ -90,7 +92,7 @@ contract ADS {
     }
     
     function NameTaken(string name) public view returns(bool) {
-        require(bytes(name).length > 0);
+        require(bytes(name).length > 0 && bytes(name).length <= 100);
         if(ContractNamesToRoutes[keccak256(name)] == 0){
             return false;//name is not taken
         }else{
