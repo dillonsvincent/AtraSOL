49a9dca606875d31ccbafc4448815dd60cbca616
Dillon Vincent <dvincent@douche-bag.local>
2018-04-27 21:55:43 -0700

    refactor file structure

--- COS/Iatra-cos.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity ^0.4.19;
-
-// What do we want out of the owner contract?
-// set ourself of the owner of a contract
-// transfer ownership of contract
-
-interface IAtraCos {
-	function SetOwnerOfContract() public returns(bool);
-}
